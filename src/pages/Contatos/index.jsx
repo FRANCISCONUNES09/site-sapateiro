5ca95f296834155c81abc1a1ed3e79e229bd33a1
FRANCISCONUNES09 <francisconunes76558@gmail.com>
2025-06-03 20:06:01 -0300

    Conserto de calçados

--- src/pages/Contatos/index.jsx
+++ src/pages/Contatos/index.jsx
@@ -25,7 +25,7 @@ export default function Contatos() {
           Entre em Contato
         </h1>
         <p className="text-gray-700 mb-6">
-          Estamos localizados no Center Um dentro do estacionamento, lado do Del Passeo da Aldeota,
+          Estamos localizados no Shopping Center Um dentro do estacionamento, lado do Del Passeo, da Aldeota,
           Fortaleza. Se você tiver dúvidas, sugestões ou quiser fazer parte do
           nosso projeto, entre em contato pelos canais abaixo:
         </p>
@@ -38,7 +38,10 @@ export default function Contatos() {
             <strong>Email:</strong> contato@barão.org.br
           </p>
           <p>
-            <strong>Endereço:</strong>  Center Um, Avenida Santos
+            <strong>Horário de Funcionamento:</strong> Segunda a Sábado, das 9h às 19h
+          </p>
+          <p>
+            <strong>Endereço:</strong>  Shopping Center Um, Avenida Santos
             Dumont, 3130, loja 15 - Aldeota, Fortaleza - CE
           </p>
         </div>
@@ -55,7 +58,7 @@ export default function Contatos() {
 
       <div className="w-full md:w-1/2 h-96 md:h-auto">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.616226256544!2d-46.65598108440661!3d-23.5841310685784!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce59c7a4e458a1%3A0x2b61c24f8dc67e2e!2sEdif%C3%ADcio%20Manhattan%20-%20Aldeota!5e0!3m2!1spt-BR!2sbr!4v1716654321013!5m2!1spt-BR!2sbr"
+          src="https://www.google.com/maps/embed?origin=mfe&pb=!1m2!2m1!1sAv.+Santos+Dumont,+3130+-+Loja+15,+Aldeota,+Fortaleza+-+CE"
           width="100%"
           height="100%"
           style={{ border: 0 }}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,7 +8,7 @@ export default function Home() {
         <p className="text-[20px] text-[#8a898c]">conserto de calçados.</p>
       </div>
       <div className="w-full h-auto flex flex-wrap justify-center gap-2 pt-8">
-        <p>ola</p>
+        <img src="Imagem do Barão.jpg" alt="" />
       </div>
     </PageWrapper>
   )
