afea6f2c9cf6b1218e8acfd2a6c7b6595c22b2df
FRANCISCONUNES09 <francisconunes76558@gmail.com>
2025-06-24 22:57:49 -0300

    img

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import PageWrapper from "@/components/pageWrapper";
 export default function Home() {
   return (
     <PageWrapper>
-      <div className="w-full flex flex-col items-center justify-center pt-7">
+      <div className="w-full flex flex-col items-center justify-center pt-7 p-4">
         <h1 className="text-[40px] text-[red] font-bold">Barão</h1>
         <p className="text-[20px] text-[#8a898c]">Venha conhecer nossos serviços!</p>
       </div>
